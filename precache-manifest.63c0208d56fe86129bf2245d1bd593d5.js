133e3a23c0003cc30335ebb1d1d4c3e0f209df71
Jasmeet KAUR <kaur.jasmeet1708@gmail.com>
2021-02-16 01:09:34 -0500

    Updates

---
+++ precache-manifest.63c0208d56fe86129bf2245d1bd593d5.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e1aca8f6136ac2903e32d65cc600b1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6f21a560599bc83fffa",
+    "url": "/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "3246b63882a0bf6b45e6",
+    "url": "/static/css/4.3e7d09e6.chunk.css"
+  },
+  {
+    "revision": "eadf4f32fc09658cdc98",
+    "url": "/static/css/main.c29e7c25.chunk.css"
+  },
+  {
+    "revision": "f0401f52d191b15fa25f",
+    "url": "/static/js/2.4a7de355.chunk.js"
+  },
+  {
+    "revision": "0a6d64cd3b49be6a14afdf21fa7c6d43",
+    "url": "/static/js/2.4a7de355.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6f21a560599bc83fffa",
+    "url": "/static/js/3.9492b5c9.chunk.js"
+  },
+  {
+    "revision": "3246b63882a0bf6b45e6",
+    "url": "/static/js/4.87acad9c.chunk.js"
+  },
+  {
+    "revision": "eadf4f32fc09658cdc98",
+    "url": "/static/js/main.3ee5c1b5.chunk.js"
+  },
+  {
+    "revision": "e3da9f3eef3280ade0b7",
+    "url": "/static/js/runtime-main.a2aa26e6.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c30bd1a881229b007d840b6f5c31f739",
+    "url": "/static/media/Quovantis.c30bd1a8.png"
+  },
+  {
+    "revision": "ffdaca3996df4df20da20f047f260bb3",
+    "url": "/static/media/York_square.ffdaca39.jpg"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "3d37b8ef6f3e178dc7075f08e312b44e",
+    "url": "/static/media/jiit.3d37b8ef.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "b400f2a2cf0dcae71f301720db923be9",
+    "url": "/static/media/pointclickcare_logo.b400f2a2.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "739e19286b19b7d574bd7b8b40b9550b",
+    "url": "/static/media/zipp-sliplogo.739e1928.png"
+  },
+  {
+    "revision": "8ace2d4449bd3aa4c55ba79693def3b8",
+    "url": "/static/media/zopper.8ace2d44.jpg"
+  }
+]);
